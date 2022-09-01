861e1822a6d3050be6ecffca24024c911be80b6b
Ashish yadav <ashish.yadav@sunfox.in>
2022-09-01 14:51:38 +0530

    create a method that fetch data from data base first if not found then fetch it from the server.

--- app/src/main/java/com/example/daggerhiltpoc/MainActivity.kt
+++ app/src/main/java/com/example/daggerhiltpoc/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.daggerhiltpoc
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.example.daggerhiltpoc.util.Status
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -24,24 +25,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.output_text_view)
+
+        viewModel.getUserFromRepo()
+
         viewModel.res.observe(this){ response ->
             CoroutineScope(IO).launch {
                 withContext(Dispatchers.Default) {
                     response.data?.forEach {
-                        viewModel.saveUserInDB(it)
-                    }
-                }
-            }
-            runOnUiThread {
-                when (response.status) {
-                    Status.SUCCESS -> {
-                        textView.text = response.data!![0].email
-                    }
-                    Status.ERROR -> {
-                        textView.text = response.message
-                    }
-                    else -> {
-                        textView.text = getString(R.string.loading_message)
+                        Log.d(TAG, "onCreate: ${it.name}")
                     }
                 }
             }
